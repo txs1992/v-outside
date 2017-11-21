444929b54960db37e79c91394653546b57183c79
stx <xusheng.tao@ele.me>
2017-11-21 14:50:52 +0800

    修改 rollup 打包配置

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,11 @@
 import { minify } from 'uglify-es'
 import uglify from 'rollup-plugin-uglify'
 import eslint from 'rollup-plugin-eslint'
+import commonjs from 'rollup-plugin-commonjs'
 import resolve from 'rollup-plugin-node-resolve'
 
 export default {
-  name: 'FocusOutside',
+  name: 'VueFocusOutside',
   entry: 'index.js',
   format: 'umd',
   dest: 'lib/index.js',
@@ -12,6 +13,10 @@ export default {
     eslint({
       throwError: true,
       exclude: 'node_modules/**'
+    }),
+    commonjs({
+      include: 'node_modules/**',
+      namedExports: { 'focus-outside': ['bind', 'unbind' ] }
     }),
     resolve({
       extensions: ['.js']
